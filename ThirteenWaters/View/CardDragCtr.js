3e67129885a950d1f35242b3cce05a2c775391e7
tony <994262676@qq.com>
2017-03-30 13:04:36 +0800

    --

--- ThirteenWaters/View/CardDragCtr.js
+++ ThirteenWaters/View/CardDragCtr.js
@@ -4,3 +4,20 @@
  * time : 2017/03/24
  */
 
+module.exports =  cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        /**碰撞组 1*/
+        group1:[cc.Node],
+        /**碰撞组 2*/
+        group2:[cc.Node],
+        /**碰撞组 3*/
+        group3:[cc.Node]
+    },  
+
+    /***检查碰撞 */
+    checkTouchPos : function(position){
+        return 1;
+    }
+});
